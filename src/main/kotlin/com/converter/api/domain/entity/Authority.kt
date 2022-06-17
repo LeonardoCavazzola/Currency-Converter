18e225d0367fedddc3f4a38742c1b1aeb5d5d90c
LeonardoCavazzola <ext_lejunior@mercadolivre.com>
2022-06-17 12:37:15 -0300

    Remove unnecessary properties

--- src/main/kotlin/com/converter/api/domain/entity/Authority.kt
+++
@@ -1,6 +0,0 @@
-package com.converter.api.domain.entity
-
-data class Authority(
-    val id: Long? = null,
-    val nome: String,
-)
--- src/main/kotlin/com/converter/api/domain/entity/User.kt
+++ src/main/kotlin/com/converter/api/domain/entity/User.kt
@@ -4,5 +4,4 @@ data class User(
     val id: Long? = null,
     val email: String,
     val password: String,
-    val authorities: List<Authority> = emptyList()
 )
