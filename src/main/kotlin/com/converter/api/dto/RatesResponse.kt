203f878562c13a339f90badba37e5970102d919a
LeonardoCavazzola <ext_lejunior@mercadolivre.com>
2022-06-30 18:36:58 -0300

    add Rates client

--- src/main/kotlin/com/converter/api/dto/RatesResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.converter.api.dto
-
-import java.math.BigDecimal
-
-data class RatesResponse(
-    val success: Boolean,
-    val base: String,
-    val rates: Map<String, BigDecimal>
-)
