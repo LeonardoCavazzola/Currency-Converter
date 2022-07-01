2f08471f31e3bbc5c220523339b0e8c7ad6fd0d5
LeonardoCavazzola <ext_lejunior@mercadolivre.com>
2022-06-30 21:04:43 -0300

    add save to user repository

--- src/main/kotlin/com/converter/api/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/converter/api/domain/repository/UserRepository.kt
@@ -4,5 +4,6 @@ import com.converter.api.domain.entity.User
 
 interface UserRepository {
     fun findByEmail(email: String): User?
+    fun save(user: User): User
     fun existsByEmail(email: String): Boolean
 }
