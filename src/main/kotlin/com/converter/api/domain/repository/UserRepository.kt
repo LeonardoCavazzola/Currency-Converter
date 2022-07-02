e5b386d8b090f6f026a49e94b13df753cb3ade75
LeonardoCavazzola <ext_lejunior@mercadolivre.com>
2022-07-02 14:30:34 -0300

    update repositories

--- src/main/kotlin/com/converter/api/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/converter/api/domain/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package com.converter.api.domain.repository
 import com.converter.api.domain.entity.User
 
 interface UserRepository {
+    fun findById(id: Long): User?
     fun findByEmail(email: String): User?
     fun save(user: User): User
     fun existsByEmail(email: String): Boolean
--- src/main/kotlin/com/converter/api/infra/dataBase/repository/UserRepositoryImp.kt
+++ src/main/kotlin/com/converter/api/infra/dataBase/repository/UserRepositoryImp.kt
@@ -6,12 +6,15 @@ import com.converter.api.infra.dataBase.model.UserModel
 import com.converter.api.infra.dataBase.model.toEntity
 import com.converter.api.infra.dataBase.model.toModel
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class UserRepositoryImp(
     val modelRepository: UserModelRepository
 ) : UserRepository {
+    override fun findById(id: Long): User? = modelRepository.findByIdOrNull(id)?.toEntity()
+
     override fun findByEmail(email: String): User? = modelRepository.findByEmail(email)?.toEntity()
 
     override fun save(user: User): User = user.also { modelRepository.save(it.toModel()) }
