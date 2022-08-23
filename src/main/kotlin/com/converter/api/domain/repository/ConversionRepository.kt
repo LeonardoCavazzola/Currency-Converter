31fee022377780ca5c105ad30c82cbb2f46971f9
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-23 15:14:16 -0300

    update conversion repository to use only the user id

--- src/main/kotlin/com/converter/api/domain/repository/ConversionRepository.kt
+++ src/main/kotlin/com/converter/api/domain/repository/ConversionRepository.kt
@@ -1,11 +1,10 @@
 package com.converter.api.domain.repository
 
 import com.converter.api.domain.entity.Conversion
-import com.converter.api.domain.entity.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 interface ConversionRepository {
     fun save(conversion: Conversion): Conversion
-    fun findAllByUser(user: User, pageable: Pageable): Page<Conversion>
+    fun findAllByUserId(userId: String, pageable: Pageable): Page<Conversion>
 }
--- src/main/kotlin/com/converter/api/infra/dataBase/repository/ConversionRepositoryImp.kt
+++ src/main/kotlin/com/converter/api/infra/dataBase/repository/ConversionRepositoryImp.kt
@@ -1,10 +1,8 @@
 package com.converter.api.infra.dataBase.repository
 
 import com.converter.api.domain.entity.Conversion
-import com.converter.api.domain.entity.User
 import com.converter.api.domain.repository.ConversionRepository
 import com.converter.api.infra.dataBase.model.ConversionModel
-import com.converter.api.infra.dataBase.model.UserModel
 import com.converter.api.infra.dataBase.model.toEntity
 import com.converter.api.infra.dataBase.model.toModel
 import org.springframework.data.domain.Page
@@ -16,11 +14,12 @@ import org.springframework.stereotype.Repository
 class ConversionRepositoryImp(
     val modelRepository: ConversionModelRepository
 ) : ConversionRepository {
-    override fun save(conversion: Conversion): Conversion = conversion.also { modelRepository.save(conversion.toModel()) }
-    override fun findAllByUser(user: User, pageable: Pageable): Page<Conversion> =
-        modelRepository.findAllByUser(user.toModel(), pageable).map { it.toEntity() }
+    override fun save(conversion: Conversion): Conversion = modelRepository.save(conversion.toModel()).toEntity()
+
+    override fun findAllByUserId(userId: String, pageable: Pageable): Page<Conversion> =
+        modelRepository.findAllByUserId(userId, pageable).map { it.toEntity() }
 }
 
 interface ConversionModelRepository : JpaRepository<ConversionModel, Long> {
-    fun findAllByUser(user: UserModel, pageable: Pageable): Page<ConversionModel>
+    fun findAllByUserId(user: String, pageable: Pageable): Page<ConversionModel>
 }
