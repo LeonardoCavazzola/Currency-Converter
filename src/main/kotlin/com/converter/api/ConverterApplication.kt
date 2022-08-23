7cc18398f22b1fb741f84985c7966da489c56453
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-23 17:28:04 -0300

    remove trash from application start file

--- src/main/kotlin/com/converter/api/ConverterApplication.kt
+++ src/main/kotlin/com/converter/api/ConverterApplication.kt
@@ -1,37 +1,11 @@
 package com.converter.api
 
-import com.converter.api.service.UserService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
-import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class ConverterApplication {
+class ConverterApplication
 
-    @Bean
-    @Profile("dev")
-    fun generateDevUser(userService: UserService): CommandLineRunner {
-        return CommandLineRunner { args: Array<String?>? ->
-            userService.create(
-                email = "email@email.com",
-                password = "12345678"
-            )
-        }
-    }
-
-    @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplate()
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(ConverterApplication::class.java, *args)
-        }
-    }
-
+fun main(args: Array<String>) {
+    SpringApplication.run(ConverterApplication::class.java, *args)
 }
