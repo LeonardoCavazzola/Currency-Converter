312cc384f02c5994289e4360282c35e1219c6cec
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-23 15:29:12 -0300

    add rates model

--- src/test/kotlin/com/converter/api/mocks/RatesMocks.kt
+++ src/test/kotlin/com/converter/api/mocks/RatesMocks.kt
@@ -1,9 +1,19 @@
 package com.converter.api.mocks
 
+import com.converter.api.domain.entity.Rates
 import com.converter.api.infra.gateway.model.RatesResponse
 import java.math.BigDecimal
 
 object RatesMocks {
+    fun entity() = Rates(
+        success = true,
+        base = "EUR",
+        rates = mapOf(
+            "BRL" to BigDecimal("5"),
+            "USD" to BigDecimal("2"),
+        ),
+    )
+
     fun model() = RatesResponse(
         success = true,
         base = "EUR",
