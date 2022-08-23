17a31ef83b33964f74a386a2875d37703d8d9eb5
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-23 17:25:39 -0300

    add user id filter

---
+++ src/main/kotlin/com/converter/api/config/UserIdFilter.kt
@@ -0,0 +1,28 @@
+package com.converter.api.config
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class UserIdFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        request.getHeader(USER_ID_HEADER)?.let {
+            val authentication = PreAuthenticatedAuthenticationToken(it, null)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+    }
+
+    companion object {
+        private const val USER_ID_HEADER = "user_id"
+    }
+}
+
