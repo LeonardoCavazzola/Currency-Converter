e67853f90ed0dc61f5d59288e4f5cfea9d1c52ce
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-23 15:05:05 -0300

    add static factory method test

--- src/test/kotlin/com/converter/api/domain/entity/ConversionTest.kt
+++ src/test/kotlin/com/converter/api/domain/entity/ConversionTest.kt
@@ -1,9 +1,11 @@
 package com.converter.api.domain.entity
 
+import com.converter.api.mocks.ConversionIntentMocks
 import com.converter.api.mocks.ConversionMocks
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
+import java.math.MathContext
 
 internal class ConversionTest {
     @Test
@@ -16,5 +18,22 @@ internal class ConversionTest {
 
         // assert
         assertEquals(BigDecimal("15.5654654610"), result)
+    }
+
+    @Test
+    fun `given a conversionIntent, originRate and a destinyRate, when fromIntent, should return a Conversion`() {
+        // given
+        val intent = ConversionIntentMocks.entity()
+        val originRate = BigDecimal("2")
+        val destinyRate = BigDecimal("9")
+
+        // when
+        val result = Conversion.fromIntent(intent, originRate, destinyRate)
+
+        // assert
+        assertEquals(intent.originCurrency, result.originCurrency)
+        assertEquals(intent.originValue, result.originValue)
+        assertEquals(intent.destinyCurrency, result.destinyCurrency)
+        assertEquals(destinyRate.divide(originRate, MathContext.DECIMAL128), result.conversionRate)
     }
 }
