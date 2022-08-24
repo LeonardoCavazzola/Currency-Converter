fd3a65de9f77d659f1452f36252e198a1245bdb7
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-24 15:01:04 -0300

    update conversion controller to use the new service model

--- src/main/kotlin/com/converter/api/controller/ConversionController.kt
+++ src/main/kotlin/com/converter/api/controller/ConversionController.kt
@@ -1,85 +1,40 @@
 package com.converter.api.controller
 
+import com.converter.api.domain.service.ConversionService
+import com.converter.api.controller.dto.ConversionIntentInput
+import com.converter.api.controller.dto.ConversionOutput
+import com.converter.api.controller.dto.toEntity
+import com.converter.api.controller.dto.toOutput
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
-import com.converter.api.components.hateoas.ConversionLinkFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.util.UriComponentsBuilder
-import org.springframework.hateoas.PagedModel
-import org.springframework.hateoas.EntityModel
-import com.converter.api.dto.ConversionView
-import org.springframework.data.web.PagedResourcesAssembler
-import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import javax.validation.Valid
-import com.converter.api.dto.ConversionForm
-import com.converter.api.model.Conversion
-import com.converter.api.service.ConversionService
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
-import javax.transaction.Transactional
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.util.UriComponentsBuilder
+import javax.validation.Valid
 
 @RestController
-@RequestMapping("/convertions")
+@RequestMapping("/conversions")
 class ConversionController(
     private val conversionService: ConversionService,
-    private val conversionLinkFactory: ConversionLinkFactory
 ) {
-
     @GetMapping
-    fun getAll(
-        pageable: Pageable? = null,
-        builder: UriComponentsBuilder? = null
-    ): PagedModel<EntityModel<ConversionView>> {
-
-        val page = conversionService.getMyConvetions(pageable)
-            .map { conversion: Conversion ->
-                ConversionView(
-                    conversion = conversion
-                )
-            }
-
-        val pra = PagedResourcesAssembler<ConversionView>(
-            HateoasPageableHandlerMethodArgumentResolver(),
-            builder?.path("/convertions")?.build()
-        )
-
-        val pagedModel = pra.toModel(page)
-        pagedModel.add(conversionLinkFactory.convert())
-
-        return pagedModel
-    }
+    fun getAll(pageable: Pageable): Page<ConversionOutput> =
+        conversionService.getMyConversions(pageable).map { it.toOutput() }
 
     @PostMapping
-    @Transactional
     fun convert(
-        @RequestBody @Valid form: ConversionForm? = null,
-        builder: UriComponentsBuilder? = null
-    ): ResponseEntity<ConversionView> {
-
-        var converted = conversionService.convert(
-            originCurrency = form!!.originCurrency!!,
-            destinyCurrency = form.destinyCurrency!!,
-            originValue = form.originValue!!
-        )
-
-        converted = conversionService.create(
-            conversion = converted
-        )
-
-        val conversionView = ConversionView(
-            conversion = converted
-        )
-
+        @RequestBody @Valid input: ConversionIntentInput,
+        builder: UriComponentsBuilder
+    ): ResponseEntity<ConversionOutput> {
+        val conversionOutput = conversionService.convert(input.toEntity()).toOutput()
         val uri = builder
-            ?.path("/convertions")
-            ?.buildAndExpand(converted.id)
-            ?.toUri()
-
-        conversionView.add(conversionLinkFactory.getAllMyConversions())
-        conversionView.add(conversionLinkFactory.convert())
-
-        return ResponseEntity.created(uri!!).body(conversionView)
+            .path("/conversions")
+            .buildAndExpand(conversionOutput.id)
+            .toUri()
+        return ResponseEntity.created(uri).body(conversionOutput)
     }
 }
