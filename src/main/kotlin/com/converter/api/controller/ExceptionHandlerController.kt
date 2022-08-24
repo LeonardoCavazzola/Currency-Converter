312400c8732e722ad79b7273b0f350981b60ac41
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-08-24 15:08:13 -0300

    update exception handler to use the best practices

--- src/main/kotlin/com/converter/api/controller/ExceptionHandlerController.kt
+++ src/main/kotlin/com/converter/api/controller/ExceptionHandlerController.kt
@@ -1,52 +1,31 @@
 package com.converter.api.controller
 
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import com.converter.api.controller.dto.ExceptionOutput
+import com.converter.api.exception.CurrencyDoesntExistOrArentAvailableException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.MethodArgumentNotValidException
-import com.converter.api.controller.dto.ExceptionOutput
-import org.springframework.validation.FieldError
-import com.converter.api.exception.CurrencyDontExistOrArentAvailableException
-import com.converter.api.exception.TokenException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
-import java.util.ArrayList
-import java.util.function.Consumer
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandlerController {
-
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handle(exception: MethodArgumentNotValidException): List<ExceptionOutput> {
-
-        val dto: MutableList<ExceptionOutput> = ArrayList()
         val fieldErrors = exception.bindingResult.fieldErrors
-
-        fieldErrors.forEach(Consumer { e: FieldError ->
-            val error = ExceptionOutput(
-                field = e.field,
-                error = e.defaultMessage!!
+        return fieldErrors.map {
+            ExceptionOutput(
+                field = it.field,
+                error = it.defaultMessage!!
             )
-            dto.add(error)
-        })
-        return dto
+        }
     }
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(CurrencyDontExistOrArentAvailableException::class)
-    fun handle(exception: CurrencyDontExistOrArentAvailableException): ExceptionOutput {
-        return ExceptionOutput(
-            field = "originCurrency and/or destinyCurrency",
-            error = exception.message
-        )
-    }
-
-    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(TokenException::class)
-    fun handle(exception: TokenException): ExceptionOutput {
-        return ExceptionOutput(
-            field = "Bearer Token",
-            error = exception.message
-        )
-    }
+    @ExceptionHandler(CurrencyDoesntExistOrArentAvailableException::class)
+    fun handle(exception: CurrencyDoesntExistOrArentAvailableException): ExceptionOutput = ExceptionOutput(
+        field = "originCurrency and/or destinyCurrency",
+        error = exception.message
+    )
 }
